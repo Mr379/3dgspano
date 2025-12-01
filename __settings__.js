7d12f829cebc953b080c75b9effa5849979e06e5
Mr379 <44247292+Mr379@users.noreply.github.com>
2025-12-01 13:55:34 +0100

    Initial commit of PlayCanvas project files

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2374436.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 265713302, 265713308, 265713287, 265713310, 265713282, 265713293, 265713294 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
